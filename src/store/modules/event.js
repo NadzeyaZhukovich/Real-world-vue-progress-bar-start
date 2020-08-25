5e5ee5d1f8ba67c9e55839195a1764bf52450f11
nadzeya.zhukovich <nadzeya.zhukovich@trimm.nl>
2020-08-25 15:30:13 +0200

    In-Component Route (navigation) Guards

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -44,7 +44,7 @@ export const actions = {
       })
   },
   fetchEvents({ commit, dispatch }, { perPage, page }) {
-    EventService.getEvents(perPage, page)
+    return EventService.getEvents(perPage, page)
       .then(response => {
         commit('SET_EVENTS_TOTAL', parseInt(response.headers['x-total-count']))
         commit('SET_EVENTS', response.data)
